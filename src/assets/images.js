f7b1107409b02567291a12163d1c53b6fc2b4fd1
Benjamin Lee <benjamsandwich@gmail.com>
2018-05-15 14:05:01 -0700

    Add image objects with placeholder caption

---
+++ src/assets/images.js
@@ -0,0 +1,72 @@
+export const cake1 = {
+  thumbnail_url: require('./images/cake1-thumb.jpeg'),
+  url: require('./images/cake1.jpeg'),
+  caption: 'hello'
+};
+
+export const cake2 = {
+  thumbnail_url: require('./images/cake2-thumb.jpeg'),
+  url: require('./images/cake2.jpeg'),
+  caption: 'hello'
+}
+
+export const cake3 = {
+  thumbnail_url: require('./images/cake3-thumb.jpeg'),
+  url: require('./images/cake3.jpeg'),
+  caption: 'hello'
+}
+
+export const cake4 = {
+  thumbnail_url: require('./images/cake4-thumb.jpeg'),
+  url: require('./images/cake4.jpeg'),
+  caption: 'hello'
+}
+
+export const cake5 = {
+  thumbnail_url: require('./images/cake5-thumb.jpeg'),
+  url: require('./images/cake5.jpeg'),
+  caption: 'hello'
+}
+
+export const cake6 = {
+  thumbnail_url: require('./images/cake6-thumb.jpeg'),
+  url: require('./images/cake6.jpeg'),
+  caption: 'hello'
+}
+
+export const cake7 = {
+  thumbnail_url: require('./images/cake7-thumb.jpeg'),
+  url: require('./images/cake7.jpeg'),
+  caption: 'hello'
+}
+
+export const cake8 = {
+  thumbnail_url: require('./images/cake8-thumb.jpeg'),
+  url: require('./images/cake8.jpeg'),
+  caption: 'hello'
+}
+
+export const cake9 = {
+  thumbnail_url: require('./images/cake9-thumb.jpeg'),
+  url: require('./images/cake9.jpeg'),
+  caption: 'hello'
+}
+
+export const cake10 = {
+  thumbnail_url: require('./images/cake10-thumb.jpeg'),
+  url: require('./images/cake10.jpeg'),
+  caption: 'hello'
+}
+
+export const cake11 = {
+  thumbnail_url: require('./images/cake11-thumb.jpeg'),
+  url: require('./images/cake11.jpeg'),
+  caption: 'hello'
+}
+
+export const cake12 = {
+  thumbnail_url: require('./images/cake12-thumb.jpeg'),
+  url: require('./images/cake12.jpeg'),
+  caption: 'hello'
+}
+
