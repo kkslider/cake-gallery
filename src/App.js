4ef0dca28d6b57badaed7d92f84b0a4074bd06fd
Benjamin Lee <benjamsandwich@gmail.com>
2018-05-17 03:12:23 -0700

    Set a heading for gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import images from './assets/images.js';
 import PhotoGallery from './components/PhotoGallery';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <h1>cake gallery</h1>
         <PhotoGallery images={images} />
-      </div>
+      </div >
     );
   }
 }
